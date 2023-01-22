c36db5434c01586a6f0a9a715b6338ea123b86f8
Ladr00 <52217415+Ladr00@users.noreply.github.com>
2023-01-22 13:01:43 +0300

    Add files via upload

---
+++ masive-DZ2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main()
+{
+
+	//ввод с клавиатуры 
+	int n;
+	int mas [ 100 ];
+	printf("kol element ");
+	scanf("%i", &n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%i", &mas [ i ]);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%4i", mas [ i ]);
+	}
+	
+}
